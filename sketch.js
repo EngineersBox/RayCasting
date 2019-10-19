dcc8e318d76404fc83537522ee158028c525e501
EngineersBox <toastcraft.info@gmail.com>
2019-10-19 20:17:43 +1100

    Added particle, ray and wall classes. fixed casting error with dist() function

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
-function setup() {
+let walls = [];
+let particle;
 
+function setup() {
+    createCanvas(400, 400);
+    for (let i = 0; i < 5; i++) {
+        let x1 = random(width);
+        let y1 = random(height);
+        let x2 = random(width);
+        let y2 = random(height);
+        walls.push(new Wall(x1, y1, x2, y2));
+    }
+    particle = new Particle();
 }
 
 function draw() {
-    
+    background(0);
+    for (let wall of walls) {
+        wall.show();
+    }
+    particle.update(mouseX, mouseY);
+    particle.show();
+    particle.look(walls);
+    // ray.show();
+    // ray.lookat(mouseX, mouseY)
+
+    // let pt = ray.cast(wall);
+    // if (pt) {
+    //     fill(255);
+    //     ellipse(pt.x, pt.y, 8, 8);
+    // }
 }
